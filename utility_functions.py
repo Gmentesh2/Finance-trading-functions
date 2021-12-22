1aa8a6a3608808168f6d2867095e4bc446c30a72
Gmentesh2 <93538315+Gmentesh2@users.noreply.github.com>
2021-12-22 11:51:19 +0400

    sc

--- utility_functions.py
+++ utility_functions.py
@@ -35,7 +35,40 @@ def main():
 if __name__=="__main__":
     main()
 
-       
+#>>>
+
+def symbol_to_path(symbol,base_dir="data"):
+    return os.path.join(base_dir,f"{symbol}.csv")
+
+def temp(symbols,dates):
+    """Returns stock data (Open) for given symbols from CSV file"""
+    df1 = pandas.DataFrame(index=dates)
+    if "FB" not in symbols:
+        symbols.insert(0,"FB")
+    for symbol in symbols:
+        df2 = pandas.read_csv(
+            symbol_to_path(symbol),
+            index_col="Date",
+            parse_dates=True,
+            usecols=["Date","Open"],
+            na_values="nan"
+            
+        )
         
+        df2 = df2.rename(columns={"Open": symbol})
+        df1 = df1.join(df2, how="inner")
+        df1 = df1.dropna()
+    return df1
+
+def main():
+    dates = pandas.date_range("2021-11-11","2021-11-23")
+    symbols = ["DIS","AAPL","WMT"]
+    df1 = temp(symbols,dates)
+    print(df1)
+    
+if __name__=="__main__":
+    main()
+        
+    
         
         
